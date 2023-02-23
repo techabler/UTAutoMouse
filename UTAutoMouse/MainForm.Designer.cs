0456039d8af7c9e42438131fcefe6f5a192e2c0c
ygpark <ygpark@hn-tech.co.kr>
2023-02-23 16:07:45 +0900

    초기 커밋.

---
+++ UTAutoMouse/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace UTAutoMouse
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTrackingPos = new System.Windows.Forms.Button();
+            this.txtCursorPosX = new System.Windows.Forms.TextBox();
+            this.txtCursorPosY = new System.Windows.Forms.TextBox();
+            this.btnAutoClick = new System.Windows.Forms.Button();
+            this.lstPositionList = new System.Windows.Forms.ListView();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTrackingPos
+            // 
+            this.btnTrackingPos.Location = new System.Drawing.Point(248, 30);
+            this.btnTrackingPos.Name = "btnTrackingPos";
+            this.btnTrackingPos.Size = new System.Drawing.Size(94, 23);
+            this.btnTrackingPos.TabIndex = 0;
+            this.btnTrackingPos.Text = "Tracking";
+            this.btnTrackingPos.UseVisualStyleBackColor = true;
+            this.btnTrackingPos.Click += new System.EventHandler(this.btnTrackingPos_Click);
+            // 
+            // txtCursorPosX
+            // 
+            this.txtCursorPosX.Location = new System.Drawing.Point(23, 32);
+            this.txtCursorPosX.Name = "txtCursorPosX";
+            this.txtCursorPosX.ReadOnly = true;
+            this.txtCursorPosX.Size = new System.Drawing.Size(100, 21);
+            this.txtCursorPosX.TabIndex = 1;
+            // 
+            // txtCursorPosY
+            // 
+            this.txtCursorPosY.Location = new System.Drawing.Point(129, 32);
+            this.txtCursorPosY.Name = "txtCursorPosY";
+            this.txtCursorPosY.ReadOnly = true;
+            this.txtCursorPosY.Size = new System.Drawing.Size(100, 21);
+            this.txtCursorPosY.TabIndex = 2;
+            // 
+            // btnAutoClick
+            // 
+            this.btnAutoClick.Location = new System.Drawing.Point(12, 365);
+            this.btnAutoClick.Name = "btnAutoClick";
+            this.btnAutoClick.Size = new System.Drawing.Size(68, 51);
+            this.btnAutoClick.TabIndex = 3;
+            this.btnAutoClick.Text = "Auto Start";
+            this.btnAutoClick.UseVisualStyleBackColor = true;
+            this.btnAutoClick.Click += new System.EventHandler(this.btnAutoClick_Click);
+            // 
+            // lstPositionList
+            // 
+            this.lstPositionList.HideSelection = false;
+            this.lstPositionList.Location = new System.Drawing.Point(23, 89);
+            this.lstPositionList.Name = "lstPositionList";
+            this.lstPositionList.Size = new System.Drawing.Size(319, 164);
+            this.lstPositionList.TabIndex = 4;
+            this.lstPositionList.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(248, 60);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(94, 23);
+            this.btnWrite.TabIndex = 5;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(23, 62);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(206, 21);
+            this.txtTitle.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 428);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.lstPositionList);
+            this.Controls.Add(this.btnAutoClick);
+            this.Controls.Add(this.txtCursorPosY);
+            this.Controls.Add(this.txtCursorPosX);
+            this.Controls.Add(this.btnTrackingPos);
+            this.Name = "MainForm";
+            this.Text = "Auto Mousor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTrackingPos;
+        private System.Windows.Forms.TextBox txtCursorPosX;
+        private System.Windows.Forms.TextBox txtCursorPosY;
+        private System.Windows.Forms.Button btnAutoClick;
+        private System.Windows.Forms.ListView lstPositionList;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.TextBox txtTitle;
+    }
+}
+
---
+++ UTAutoMouse/MainForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+
+namespace UTAutoMouse
+{
+    /// <summary>
+    /// [REF]
+    /// 1. Auto Click : https://diy-dev-design.tistory.com/13
+    /// 2. Cross thread exception : https://manniz.tistory.com/entry/CC-Sharp-%ED%81%AC%EB%A1%9C%EC%8A%A4-%EC%8A%A4%EB%A0%88%EB%93%9C-%EC%9E%91%EC%97%85%EC%9D%B4-%EC%9E%98%EB%AA%BB%EB%90%98%EC%97%88%EC%8A%B5%EB%8B%88%EB%8B%A4-%EB%B0%94%EB%A1%9C-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B8%B0
+    /// 3. Get Mouse Position : https://sosal.kr/793
+    /// 4. Systime Mouse Hook : https://nowonbun.tistory.com/122
+    ///                         https://kjun.kr/1039
+    /// </summary>
+    public partial class MainForm : Form
+    {
+
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        #region [Mouse Position]
+        private const int MouseEV_Move = 0x0001;        /* mouse move 			*/
+        private const int MouseEV_LeftDown = 0x0002;    /* left button down 	*/
+        private const int MouseEV_LeftUp = 0x0004;  /* left button up 		*/
+        private const int MouseEV_RightDown = 0x0008; 	/* right button down 	*/
+
+        private int MouseSetPosX = 0;
+        private int MouseSetPosY = 0;
+
+        private int interval_;
+        private readonly ManualResetEvent stoppeing_event_ = new ManualResetEvent(false); //System.Threading;
+        #endregion
+
+        private static System.Timers.Timer aTimer;
+        private bool isRunningTracking = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Hook.GlobalEvents().MouseClick += GlobalHook_MouseClick;
+
+            lstPositionList.View = View.Details;
+            lstPositionList.GridLines = true;
+            lstPositionList.FullRowSelect = true;
+            lstPositionList.CheckBoxes = true;
+
+            lstPositionList.Columns.Clear();
+            lstPositionList.Columns.Add("Title", 150, HorizontalAlignment.Center);
+            lstPositionList.Columns.Add("X Pos", 50, HorizontalAlignment.Center);
+            lstPositionList.Columns.Add("Y Pos", 50, HorizontalAlignment.Center);
+        }
+
+        private void GlobalHook_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                btnTrackingPos_Click(null, null);
+            }
+        }
+
+        public void MouseSetPosNclick(int x, int y)
+        {
+            try
+            {
+                SetCursorPos(x, y);
+                stoppeing_event_.WaitOne(interval_);
+                MouseClick_now();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("MouseSetPosNclick\r\n" + e.Message);
+            }
+        }
+
+        public void MouseClick_now()
+        {
+            try
+            {
+                mouse_event(MouseEV_LeftDown, 0, 0, 0, 0);
+                mouse_event(MouseEV_LeftUp, 0, 0, 0, 0);
+                stoppeing_event_.WaitOne(100);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("MouseClick_now\r\n" + e.Message);
+            }
+        }
+
+        private void btnTrackingPos_Click(object sender, EventArgs e)
+        {
+            if (!isRunningTracking)
+            {
+                isRunningTracking = true;
+                aTimer = new System.Timers.Timer(100);
+                aTimer.Elapsed += ATimer_Elapsed;
+                aTimer.Enabled = true;
+            }
+            else
+            {
+                MouseSetPosX = Convert.ToInt32(txtCursorPosX.Text);
+                MouseSetPosY = Convert.ToInt32(txtCursorPosY.Text);
+
+                isRunningTracking = false;
+                aTimer.Enabled = false;
+            }
+        }
+
+        private void ATimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            txtCursorPosX.Text = Cursor.Position.X.ToString(); 
+            txtCursorPosY.Text = Cursor.Position.Y.ToString();
+
+
+        }
+
+        private void btnAutoClick_Click(object sender, EventArgs e)
+        {
+            MouseSetPosNclick(MouseSetPosX, MouseSetPosY);
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            ListViewItem lv = new ListViewItem(txtTitle.Text);
+            lv.SubItems.Add(MouseSetPosX.ToString());
+            lv.SubItems.Add(MouseSetPosY.ToString());
+            lstPositionList.Items.Add(lv);
+
+            txtTitle.Text = String.Empty;
+
+        }
+
+        public class CPosition
+        {
+            public string title { get; set; }
+            public int posX { get; set; }
+            public int posY { get; set; }
+        }
+
+    }
+}
